d1ec2ff6d0b23d5d9141291d7b7c38cbc1fe904a
SorenAaenMadsen <saaenmadsen@gmail.com>
2024-10-30 14:58:29 +0100

    Building company flawor texts with AI

--- src/main/java/com/saaenmadsen/shardworld/aitools/AiTextGenerator.java
+++ src/main/java/com/saaenmadsen/shardworld/aitools/AiTextGenerator.java
@@ -5,7 +5,7 @@ import dev.langchain4j.model.ollama.OllamaChatModel;
 
 public class AiTextGenerator {
     private static final String LOCALHOST = "http://localhost:11434";
-    private static final String modelName = "gemma2:2b";
+    public static final String modelName = "gemma2:2b";
 
     public static ChatLanguageModel connectModel() {
         return OllamaChatModel.builder()
--- src/main/java/com/saaenmadsen/shardworld/constants/companies/Company.java
+++ src/main/java/com/saaenmadsen/shardworld/constants/companies/Company.java
@@ -29,23 +29,23 @@ public class Company {
 
         String productionProcesses = Arrays.stream(knownRecipes).map(knownRecipes -> knownRecipes.getProcessDescription()).collect(Collectors.joining(", "));
 
-        String request = "Choose a random good enough company name for a company producing by " + productionProcesses + " and make it a short answer with just one name.";
+        String companyDescription = "a company producing by " + productionProcesses + "";
 
 
-        this.companyName = chatLanguageModel.generate(request).trim();
-        this.companyMarketingMotto = chatLanguageModel.generate("Make a company marketing slogan for that company, using max 120 characters. Return just that marketing motto as a plain string and nothing else.").trim();
-        this.leadershipStyle = chatLanguageModel.generate("Describe the leadership style of that company in 200 characters, with the added knowledge that it is " + culture.getInnovativenessLevel().getDescription() + ". Return just the leadership style description as a string.").trim();
+        this.companyName = chatLanguageModel.generate("Choose a random good enough company name for " + companyDescription + " and make it a short answer with just one name.").trim();
+        this.companyMarketingMotto = chatLanguageModel.generate("Make a company marketing slogan for "+ companyDescription +", using max 120 characters. Return just that marketing motto as a plain string and nothing else.").trim();
+        this.leadershipStyle = chatLanguageModel.generate("Describe the leadership style of "+ companyDescription +" in 200 characters, with the added knowledge that it is " + culture.getInnovativenessLevel().getDescription() + ". Return just the leadership style description as a string.").trim();
 
     }
 
     @Override
     public String toString() {
         return "Company{" +
-                "companyName='" + companyName + '\'' +
-                ", companyMarketingMotto='" + companyMarketingMotto + '\'' +
-                ", leadershipStyle='" + leadershipStyle + '\'' +
-                ", culture=" + culture +
-                ", knownRecipes=" + Arrays.toString(knownRecipes) +
-                '}';
+                "\n   companyName='" + companyName + '\'' +
+                ", \n   companyMarketingMotto='" + companyMarketingMotto + '\'' +
+                ", \n   leadershipStyle='" + leadershipStyle + '\'' +
+                ", \n   culture=" + culture +
+                ", \n   knownRecipes=" + Arrays.toString(knownRecipes) +
+                "\n}";
     }
 }
