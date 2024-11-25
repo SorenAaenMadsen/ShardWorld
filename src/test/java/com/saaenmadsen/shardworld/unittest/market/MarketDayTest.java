9d9ecfa822a65a632e42936c60a43ba6e37b9d33
SorenAaenMadsen <saaenmadsen@gmail.com>
2024-11-25 10:44:25 +0100

    Fixing unittest

--- src/test/java/com/saaenmadsen/shardworld/unittest/market/MarketDayTest.java
+++ src/test/java/com/saaenmadsen/shardworld/unittest/market/MarketDayTest.java
@@ -10,69 +10,69 @@ public class MarketDayTest {
     @Test
     public void testPriceAdjustmentsNothingSold() {
         assertThat("Lots for sale, nothing was sold",
-                MarketDay.calculateNewPrice(1000, 1000, 1000, 0),
+                MarketDay.calculateNewPrice(1000, 1000, 1000, 0, 2000),
                 equalTo(800));
     }
 
     @Test
     public void testPriceAdjustmentsDemandsMeetExactly() {
         assertThat("Lots for sale, and all is sold. No customers unfulfilled. B-E-A-utiful market day",
-                MarketDay.calculateNewPrice(1000, 1000, 0, 0),
+                MarketDay.calculateNewPrice(1000, 1000, 0, 0, 2000),
                 equalTo(1000));
     }
 
     @Test
     public void testOneUnsold() {
         assertThat("To be testOneUnsold",
-                MarketDay.calculateNewPrice(1000, 1000, 1, 0),
+                MarketDay.calculateNewPrice(1000, 1000, 1, 0, 2000),
                 equalTo(1000));
     }
 
     @Test
     public void testOneUnfulfilled() {
         assertThat("To be testOneUnfulfilled",
-                MarketDay.calculateNewPrice(1000, 1000, 0, 1),
+                MarketDay.calculateNewPrice(1000, 1000, 0, 1, 2000),
                 equalTo(1070));
     }
 
     @Test
     public void testOneSoleMostLeftUnsold() {
         assertThat("To be testOneSoleMostLeftUnsold",
-                MarketDay.calculateNewPrice(1000, 1000, 999, 0),
+                MarketDay.calculateNewPrice(1000, 1000, 999, 0, 2000),
                 equalTo(820));
     }
 
     @Test
     public void testRequiredDoubleWhatWasAvailable() {
         assertThat("To be testRequiredDoubleWhatWasAvailable",
-                MarketDay.calculateNewPrice(1000, 1000, 0, 1000),
+                MarketDay.calculateNewPrice(1000, 1000, 0, 1000, 2000),
                 equalTo(1100));
     }
 
     @Test
     public void testRequiredTenTimesWhatWasAvailable() {
         assertThat("To be testRequiredTenTimesWhatWasAvailable",
-                MarketDay.calculateNewPrice(1000, 1000, 0, 9000),
+                MarketDay.calculateNewPrice(1000, 1000, 0, 9000, 2000),
                 equalTo(1260));
     }
 
     @Test
     public void testWhenNoneIsDemandedOrOnSale(){
         assertThat("To be described",
-                MarketDay.calculateNewPrice(1000, 0, 0, 0),
+                MarketDay.calculateNewPrice(1000, 0, 0, 0, 2000),
                 equalTo(1000));
     }
 
     @Test
     public void testPriceContinuesToFallUntilWorthless(){
         assertThat("Price must be able to fall to worthless",
-                MarketDay.calculateNewPrice(2, 1000, 1000, 0),
+                MarketDay.calculateNewPrice(2, 1000, 1000, 0, 2000),
                 equalTo(1));
         assertThat("Price must be able to fall to worthless",
-                MarketDay.calculateNewPrice(1, 1000, 1000, 0),
+                MarketDay.calculateNewPrice(1, 1000, 1000, 0, 2000),
                 equalTo(0));
         assertThat("Price cannot be negative",
-                MarketDay.calculateNewPrice(0, 1000, 1000, 0),
+                MarketDay.calculateNewPrice(0, 1000, 1000, 0, 2000),
                 equalTo(0));
     }
 
